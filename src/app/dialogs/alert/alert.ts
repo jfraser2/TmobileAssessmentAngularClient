505251e008e50bd3861a976acc13e30884d45505
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2025-09-15 20:07:43 -0500

    made the alert dialog compile

--- src/app/dialogs/alert/alert.ts
+++ src/app/dialogs/alert/alert.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import {CommonModule} from '@angular/common'; // Import NgIf
 
 @Component({
   selector: 'app-alert',
-  imports: [],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './alert.html',
   styleUrl: './alert.css'
 })
-export class Alert {
+export class Alert  implements OnInit {
+
+    messageTitle: string;
+    messageText: string;
+    messageStatus: number; // 0 - green, 1 - yellow, 2 - alert, 3 or more - red
+	
+	constructor(
+	    private dialogRef: MatDialogRef<Alert>,
+	    @Inject(MAT_DIALOG_DATA) data) {
+
+	    this.messageTitle = data.messageTitle;
+	    this.messageText = data.messageText;
+	    this.messageStatus = data.messageStatus;
+	}
+	
+	ngOnInit() {
+	}
+
+	close() {
+	    this.dialogRef.close();
+	}
 
 }
