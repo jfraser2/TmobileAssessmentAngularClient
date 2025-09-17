45f240eded758c7f8560f49c515a7490c7e57afc
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2025-09-17 00:17:18 -0500

    under contruction alert is now displaying

--- src/app/dashboards/main-dashboard/main-dashboard.ts
+++ src/app/dashboards/main-dashboard/main-dashboard.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AppDefaults } from '../../../environments/app.defaults';
 import { AlertDirective } from '../../directives/alert-directive';
-
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
   selector: 'app-main-dashboard',
   imports: [],
   templateUrl: './main-dashboard.html',
-  styleUrl: './main-dashboard.css'
+  styleUrl: './main-dashboard.css',
+  providers: [AlertDirective]  
 })
 export class MainDashboard  implements OnInit {
 
