a757230468a9451ce481232df8cf7670321b0b28
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2025-09-23 21:27:50 -0500

    code improvement to handle matTable with async data fetch

--- src/app/cards/task/list/list-all.ts
+++ src/app/cards/task/list/list-all.ts
@@ -1,57 +1,56 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule for ngIf
 import { AlertDirective } from '../../../directives/alert-directive';
 import { ListAllTasks } from '../../../services/task/list-all-tasks';
 import { AppDefaults } from '../../../../environments/app.defaults';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatSortModule, MatSort } from '@angular/material/sort';
 import { TaskRow } from '../../../models/task-row';
 
 @Component({
   selector: 'app-list-all',
-  imports: [CommonModule, MatTableModule, ScrollingModule, MatPaginatorModule],
+  imports: [CommonModule, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './list-all.html',
   styleUrl: './list-all.css',
-  providers: [ListAllTasks, AlertDirective]  
+  providers: [ListAllTasks, AlertDirective, MatSort]  
 })
-export class ListAll implements OnInit {
+export class ListAll implements OnInit, AfterViewInit {
 	
 	sectionTitle: string;
-	loading = false;
+	loading : boolean = false;
 	appDef = AppDefaults;
 	public isLoaded : boolean =  false;	
 	public totalRows : number = 0;
 	matColumnDefIds : string[];
 	taskJavascriptArrayData: TaskRow[];
-	dataSource = null;
-	@ViewChild(MatSort) sort!: MatSort;		
+	dataSource : MatTableDataSource<TaskRow> =  new MatTableDataSource<TaskRow>();
+	@ViewChild(MatSort, {static: true}) sort!: MatSort;	
+			
 
 	constructor(public listAllTasksService: ListAllTasks, public alertDirective: AlertDirective) {
 	  this.sectionTitle ="List All Tasks";
       this.matColumnDefIds = ['id', 'taskName', 'taskDescription', 'taskStatus', 'taskCreateDate']; // Define the matColumnDefIds
 	}
 
-	async ngOnInit() {
-	/* await makes the process act like it is synchronous, it can only be run in a async function  */
-  	  let tempPromise = await this.executeFindAllTasks();
-  	  if (this.taskJavascriptArrayData === null) {
-		console.log("Search had an Error!!!!");
-		this.totalRows = 0;
-		this.dataSource = new MatTableDataSource<TaskRow>();
-		this.dataSource.sort = this.sort;
-		this.isLoaded = true;
-  	  } else {
-  		console.log("Search did not have an error");
-  		this.dataSource = new MatTableDataSource<TaskRow>(this.taskJavascriptArrayData);
-  		this.dataSource.sort = this.sort;
-  		this.totalRows = this.taskJavascriptArrayData.length;
-  	    this.isLoaded = true;
-  	  }
-	  
+	ngOnInit() {
+  	  let tempPromise = this.executeFindAllTasks();
 	}
+	
+	ngAfterViewInit() {
+	  if (null != this.dataSource) {	
+	    if (null !== this.sort) {
+		  this.dataSource.sort = this.sort;
+		  console.log("Sort is not null");
+	    } else {
+		  console.log("Sort is null");
+	    }
+	  } else {
+		console.log("dataSource is null");
+	  }	
+	}  	
 
+	/* a Promise execution is asynchronous */
 	executeFindAllTasks(): Promise<any> {
 
 	    const findAllTasksPromise = this.listAllTasksService.getAllTasksPromise();
@@ -62,6 +61,10 @@ export class ListAll implements OnInit {
 	            /* good Result res is javascript */
 				this.taskJavascriptArrayData =  res.TaskEntity;
 	            this.loading = false;
+				console.log("Search did not have an error");
+				this.dataSource.data = this.taskJavascriptArrayData;
+				this.totalRows = this.taskJavascriptArrayData.length;
+				this.isLoaded = true;
 	        },
 	        (err) => { // Error err is a javascript object
 				this.taskJavascriptArrayData =  null;
@@ -69,6 +72,10 @@ export class ListAll implements OnInit {
 	            /* status values: 0 - green, 1 - yellow, 2 - alert, 3 or more - red */
 	            this.alertDirective.openDialog('List All Tasks Error', errMessage, 3);
 	            this.loading = false;
+				console.log("Search had an Error!!!!");
+				this.dataSource.data = null;
+				this.totalRows = 0;
+				this.isLoaded = true;
 	        }
 	    ); // end the then function
 
